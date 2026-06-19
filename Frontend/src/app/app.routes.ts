c26573e183a7397a45d10aec42658805591ca745
Mar Flores <nemiflores1@gmail.com>
2026-06-18 19:13:19 -0600

    Login
    La estructura va tomando forma, se tiene un icono propio de la tienda, el login inicia sesion con datos locales previamente definidos y redirige al gerente/cajero a su respectivo dashboard

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Login } from './features/auth/login/login';
+
+import { DashboardGerente } from './features/dashboard-gerente/pages/dashboard-gerente/dashboard-gerente';
+import { DashboardCajero } from './features/dashboard-cajero/pages/dashboard-cajero/dashboard-cajero';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: Login
+  },
+  {
+    path: 'dashboard-gerente',
+    component: DashboardGerente
+  },
+  {
+    path: 'dashboard-cajero',
+    component: DashboardCajero
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
--- Frontend/src/app/features/auth/login/login.ts
+++ Frontend/src/app/features/auth/login/login.ts
@@ -1,9 +1,54 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.css',
+  styleUrl: './login.css'
 })
-export class Login {}
+export class Login {
+  correo: string = '';
+  password: string = '';
+  error: string = '';
+
+  constructor(private router: Router) {}
+
+  iniciarSesion() {
+    const usuarios = [
+      {
+        correo: 'gerente@xoxo.com',
+        password: '1234',
+        rol: 'gerente'
+      },
+      {
+        correo: 'cajero@xoxo.com',
+        password: '1234',
+        rol: 'cajero'
+      }
+    ];
+
+    const usuarioEncontrado = usuarios.find(
+      usuario =>
+        usuario.correo === this.correo.trim().toLowerCase() &&
+        usuario.password === this.password
+    );
+
+    if (!usuarioEncontrado) {
+      this.error = 'Correo o contraseña incorrectos.';
+      return;
+    }
+
+    localStorage.setItem('correo', usuarioEncontrado.correo);
+    localStorage.setItem('rol', usuarioEncontrado.rol);
+
+    if (usuarioEncontrado.rol === 'gerente') {
+      this.router.navigate(['/dashboard-gerente']);
+    } else {
+      this.router.navigate(['/dashboard-cajero']);
+    }
+  }
+}
