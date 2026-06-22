71d4e4bc96ee62bd50ef2bb843025896fe6d47ed
oguzmarc14 <oguzmarc14@gmail.com>
2026-06-21 21:11:30 -0600

    feat(Productos): se agrego stock minimo para productos

--- Backend/src/models/Productos.js
+++ Backend/src/models/Productos.js
@@ -1,21 +1,26 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 const productosSchema = new mongoose.Schema({
-    nombre: {
-        type: String,
-        required: true
-    },
+  nombre: {
+    type: String,
+    required: true,
+  },
 
-    precio: {
-        type: Number, 
-        required: true
-    },
+  precio: {
+    type: Number,
+    required: true,
+  },
 
-    categoria: {
-        type: String,
-        required: true
-    },
-})
+  categoria: {
+    type: String,
+    required: true,
+  },
+
+  stockMinimo: {
+    type: Number,
+    required: true,
+    default: 5,
+  },
+});
 
 export default mongoose.model("Productos", productosSchema);
-
